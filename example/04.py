4b4116a1d2d13a247e3b7bf9325ba20b411221d5
Ultmebius <1743603522@qq.com>
2022-09-27 01:00:48 +0800

    new

--- example/04.py
+++ example/04.py
@@ -1,4 +1,7 @@
 # Prime factorization
+from tkinter import N
+
+
 def prime_factors(n):
     while n > 1:
         t = smallest_factors(n)
@@ -12,4 +15,26 @@ def smallest_factors(n):
         t = t + 1
     return t
 
-# DRY
+# 
+def sum_naturals(n):
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + k, k + 1
+    return total
+
+def identity(k):
+    return k
+
+def cube(k):
+    return pow(k, 3)
+
+from operator import mul
+
+def pi_term(k):
+    return 8 / mul(k * 4 - 3, k * 4 - 1)
+
+def summation(n, term):
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + term(k), k + 1
+    return total 
