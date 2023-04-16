89191a8c8d544050c074c3278a8fd10aae438585
Ultmebius <1743603522@qq.com>
2023-04-17 00:51:02 +0800

    delet API

--- example/chatgpt.py
+++ example/chatgpt.py
@@ -1,6 +1,6 @@
 import os
 import openai
-openai.api_key = ('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')
+openai.api_key = (' ')
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
