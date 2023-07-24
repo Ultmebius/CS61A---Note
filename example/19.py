3d587cb43179511297a836647e300cdc22878583
Ultmebius <1743603522@qq.com>
2023-07-24 10:54:07 +0800

    new

--- example/19.py
+++ example/19.py
@@ -6,7 +6,33 @@ from fractions import Fraction
 
 def str_repr_demo():
     from fractions import Fraction
+    half = Fraction(1, 2)
+    s = "hello world"
+    repr(repr(repr(s)))
+    eval(eval(eval(repr(repr(repr(s)))))) 
 
 
-half = Fraction(1, 2)
-s = "hello world"
+class Bear:
+    """A Bear.
+    
+    
+    >>> oski = Bear()
+    >>> oski
+    Bear()
+    >>> print(oski)
+    a bear
+    >>> print(str(oski))
+    a bear
+    >>> print(repr(oski))
+    Bear()
+    >>> print(oski.__repr__())
+    oski
+    >>> print(oski.__str__())
+    oski the bear
+
+    >>> print(str_(oski))
+    a bear
+    >>> print(repr_(oski))
+    Bear()
+    """
+    
