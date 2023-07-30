ad5dc6ba61e33c01d3c0f4750d126a4fc83ddc99
Ultmebius <1743603522@qq.com>
2023-07-30 15:39:34 +0800

    new

---
+++ example/20.py
@@ -0,0 +1,34 @@
+#composition
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s
+    Link(3, Link(4, Link(5)))
+    >>> print(s)
+    <3 4 5>
+    >>> s.first
+    3
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.first
+    4
+    >>> s.rest.first = 7
+    >>> s
+    Link(3, Link(7, Link(5)))
+    >>> s.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s
+    Link(6, Link(7))
+    >>> print(s)
+    <6 7>
+    >>> print(s.rest)
+    <7>
+    >>> t = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> t
+    Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print(t)
+    <1 <2 3> 4>
+    """
+    
