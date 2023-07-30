a9c7ee81e45313355023e8688bb23b3ea3d78bce
Ultmebius <1743603522@qq.com>
2023-07-30 16:12:23 +0800

    new

--- example/20.py
+++ example/20.py
@@ -1,5 +1,8 @@
 #composition
 
+from numpy import empty
+
+
 class Link:
     """A linked list.
 
@@ -31,7 +34,13 @@ class Link:
     >>> print(t)
     <1 <2 3> 4>
     """
+    empty = ()
     
+    def __init__(self, first, rest = empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+        
         
     def __repr__(self):
         if self.rest:
