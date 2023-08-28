9ea31df035005ce8774796fdbc418388c64e0233
Ultmebius <1743603522@qq.com>
2023-08-29 00:05:01 +0800

    new

--- example/20.py
+++ example/20.py
@@ -58,7 +58,43 @@ class Link:
 
 square, odd = lambda x: x * x, lambda x: x % 2 == 1
 
+def range_link(start, end):
+    """Return a Link containing consecutive integers from start to end.
 
+    >>> range_link(3, 6)
+    Link(3, Link(4, Link(5)))
+    """
+    if start >= end:
+        return Link.empty
+    else:
+        return Link(start, range_link(start + 1, end))
+
+def map_link(f, s):
+    """Return a Link that contains f(x) for each x in Link s.
+
+    >>> map_link(square, range_link(3, 6))
+    Link(9, Link(16, Link(25)))
+    """
+    if s is Link.empty:
+        return s
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+    
+def filter_link(f, s):
+    """Return a Link that contains only the elements x of Link s for which f(x)
+    is a true value.
+
+    >>> filter_link(odd, range_link(3, 6))
+    Link(3, Link(5))
+    """
+    if s is Link.empty:
+        return s
+    filtered_rest = filter_link(f, s.rest)
+    if f(s.first):
+        return Link(s.first, filtered_rest)
+    else:
+        return filtered_rest
+    
 
 t = Link(1, Link(Link(2, Link(3)), Link(4)))
 print(repr(t))
